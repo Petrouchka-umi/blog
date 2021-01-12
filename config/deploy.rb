48da3883c8bb695b166f362b0deee4050a6e4aed
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2021-01-12 09:49:23 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,9 @@
-
-set :application, "umi-blog"
-set :repo_url, "git@example.com:me/my_repo.git"
-
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'blog'
+set :application, 'umi-blog'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Petrouchka-umi/blog.git'
@@ -16,7 +12,7 @@ set :repo_url,  'git@github.com:Petrouchka-umi/blog.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.6.5です
+set :rbenv_ruby, '2.6.5' 
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
