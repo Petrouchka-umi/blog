f1d2b0017b468921a6ecf49f79a7021a28252edd
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2021-01-12 13:31:32 +0900

    fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'umi-bog'
+  config.fog_directory  = 'umi-blog'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/umi-blog'
 end
