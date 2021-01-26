2cac9126555c007b4a12b3c28207403abbbdb83e
Petrouchka-umi <67958506+Petrouchka-umi@users.noreply.github.com>
2021-01-26 16:19:27 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,8 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
+  # あとでfales
 
   config.action_controller.perform_caching = true
 
